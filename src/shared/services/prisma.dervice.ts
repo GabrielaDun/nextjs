b5424feef7e603126c2ce47fb1200bcfb4ea723c
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-11-01 09:02:44 +0100

    Added nececary prisma service code to enable and disable connection with the database

---
+++ src/shared/services/prisma.dervice.ts
@@ -0,0 +1,11 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
+// This code creatse service for Prisma.
+// It helps with initiating a connection with a database
