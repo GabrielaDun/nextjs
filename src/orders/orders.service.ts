f4502b2ffa554f8700904870c3fe84666a64b67b
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-11-02 16:25:05 +0100

    Fixed a bug

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -6,7 +6,7 @@ import { PrismaService } from 'src/shared/services/prisma.dervice';
 export class OrdersService {
   constructor(private prismaService: PrismaService) {}
   public getAll(): Promise<Order[]> {
-    return this.prismaService.order.findMany();
+    return this.prismaService.order.findMany({ include: { product: true } });
   }
   public getById(id: Order['id']): Promise<Order | null> {
     return this.prismaService.order.findUnique({
@@ -21,8 +21,14 @@ export class OrdersService {
   public create(
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
+    const { productId, ...otherData } = orderData;
     return this.prismaService.order.create({
-      data: orderData,
+      data: {
+        ...otherData,
+        product: {
+          connect: { id: productId },
+        },
+      },
     });
   }
   public edit(
