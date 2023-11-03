8a4c048c59335c1fe00fbcbcd85f3f7f49738ad0
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-11-03 08:13:18 +0100

    Finished adjusting Order service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Order } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.dervice';
 
@@ -11,6 +11,7 @@ export class OrdersService {
   public getById(id: Order['id']): Promise<Order | null> {
     return this.prismaService.order.findUnique({
       where: { id },
+      include: { product: true },
     });
   }
   public delete(id: Order['id']): Promise<Order> {
@@ -22,22 +23,34 @@ export class OrdersService {
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
     const { productId, ...otherData } = orderData;
-    return this.prismaService.order.create({
+    try {
+      return this.prismaService.order.create({
+        data: {
+          ...otherData,
+          product: {
+            connect: { id: productId },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new BadRequestException('Product does not exist');
+    }
+  }
+
+  public edit(
+    id: Order['id'],
+    orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Order> {
+    const { productId, ...otherData } = orderData;
+    return this.prismaService.order.update({
+      where: { id },
       data: {
         ...otherData,
         product: {
           connect: { id: productId },
         },
       },
-    });
-  }
-  public edit(
-    id: Order['id'],
-    orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
-  ): Promise<Order> {
-    return this.prismaService.order.update({
-      where: { id },
-      data: orderData,
     });
   }
 }
